4ee487bc7a516dcca2fae5c1228df0d827e45ab8
SYAUQIZAIDAN KHAIRAN KHALAF <syaokay@gmail.com>
2021-10-11 23:16:02 +0700

    feat: adjusting navbar on frontend section except homepage 🧙🏻‍♀️

---
+++ resources/views/layouts/frontend/minor.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="id">
+
+@include('includes.frontend.head')
+
+<body class="software-landing-page">
+
+    @yield('content')
+
+    <!--====== Start Footer ======-->
+    @include('includes.frontend.footer')
+    <!--====== End Footer ======-->
+
+    @include('includes.frontend.script')
+</body>
+
+</html>
--- resources/views/pages/frontend/team.blade.php
+++ resources/views/pages/frontend/team.blade.php
@@ -1,7 +1,95 @@
-@extends('layouts.frontend.master')
+@extends('layouts.frontend.minor')
 
 @section('title', 'Team — Collegetivity')
 @section('content')
+
+<!--====== Start Header ======-->
+<header class="template-header navbar-left sticky-header">
+    <div class="container">
+        <div class="header-inner">
+            <div class="header-left">
+                <div class="brand-logo">
+                    <a href="{{url('/')}}">
+                        <img src="{{url('landio/assets/img/logo-new-2.svg')}}" alt="logo" width="36" class="mx-auto">
+                    </a>
+                </div>
+
+                <nav class="nav-menu d-none d-xl-block">
+                    <ul>
+                        <li>
+                            <a href="{{url('/')}}">Beranda</a>
+                        </li>
+                        <li>
+                            <a href="{{url('fitur')}}">Fitur</a>
+                        </li>
+                        <li>
+                            <a href="{{url('tentang')}}">Tentang</a>
+                        </li>
+                        <li class="active">
+                            <a href="{{url('team')}}">Team</a>
+                        </li>
+                        <li>
+                            <a href="{{url('kontak')}}">Kontak</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div class="header-right">
+                <ul class="header-extra">
+                    <li class="d-none d-md-block">
+                        <a href="{{url('/register')}}" class="template-btn primary-bg-5">
+                            Daftar Sekarang <i class="fas fa-arrow-right"></i>
+                        </a>
+                    </li>
+                    <li class="d-xl-none">
+                        <a href="#" class="navbar-toggler">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <!-- Start Mobile Slide Menu -->
+    <div class="mobile-slide-panel">
+        <div class="panel-overlay"></div>
+        <div class="panel-inner">
+            <div class="mobile-logo">
+                <a href="{{url('/')}}">
+                    <img src="{{url('landio/assets/img/logo-new-2.svg')}}" width="32" alt="Logo">
+                </a>
+            </div>
+            <nav class="mobile-menu">
+                <ul>
+                    <li>
+                        <a href="{{url('/')}}">Beranda</a>
+                    </li>
+                    <li>
+                        <a href="{{url('/fitur')}}">Fitur</a>
+                    </li>
+                    <li>
+                        <a href="{{url('/tentang')}}">Tentang</a>
+                    </li>
+                    <li>
+                        <a href="{{url('/team')}}">Team</a>
+                    </li>
+                    <li>
+                        <a href="{{url('/kontak')}}">Kontak</a>
+                    </li>
+                </ul>
+            </nav>
+            <a href="#" class="panel-close">
+                <i class="fal fa-times"></i>
+            </a>
+        </div>
+    </div>
+    <!-- End Mobile Slide Menu -->
+</header>
+<!--====== End Header ======-->
 
 <!--====== Page title area Start ======-->
 <section class="page-title-area">
