73aef0e88dbdaed748960baffee89f99205a2132
SYAUQIZAIDAN KHAIRAN KHALAF <syaokay@gmail.com>
2021-10-14 18:52:39 +0700

    feat: adding htacess and schedules table 😭

---
+++ database/migrations/2021_10_11_165213_create_schedules_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_matkul', 96);
+            $table->string('nama_dosen', 48);
+            $table->string('waktu_mulai', 48);
+            $table->string('waktu_selesai', 48);
+            $table->string('hari', 16);
+            $table->integer('sks', 16);
+            $table->string('kelas', 16);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules_tables');
+    }
+}
